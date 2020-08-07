6a9918eb9e93465fa2026735ca96327b28f2df14
charliealpha094 <carlosamaral94@gmail.com>
2020-08-07 21:33:52 +0100

    The Hacker News API

---
+++ Chapter_17_Working_Api's/Hacker_News_API/hn_article.py
@@ -0,0 +1,18 @@
+#Done by Carlos Amaral (30/07/2020)
+
+
+#The hacker News API
+
+import requests
+import json
+
+#Make an API call to store the response.
+url = 'https://hacker-news.firebaseio.com/v0/item/19155826.json'
+r = requests.get(url)
+print(f"Status code: {r.status_code}")
+
+#Explore the structure of the data.
+response_dict = r.json()
+readable_file = 'data/readable_hn_data.json'
+with open(readable_file, 'w') as f:
+    json.dump(response_dict, f, indent=4)
